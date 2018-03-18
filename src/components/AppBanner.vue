26bed04950976f216a9ac4559bd4109ce289123e
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-18 18:03:59 +0300

    Banner

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="banner">
-    <div>
-      <h1>BANNER</h1>
-    </div>
+    <div/>
   </div>
 </template>
 
@@ -17,21 +15,11 @@
   display: flex;
   justify-content: center;
 }
-.banner_pink > div,
-.banner__content > div {
+.banner > div {
   opacity: .4;
   height: 90px;
   text-align: center;
-}
-.banner__content > div {
-  background: linear-gradient(to right, #1c31ef, #50ed17);
-  width: 50%;
-}
-.banner_pink > div {
   background: linear-gradient(to right, #e61cef, #ed1717);
   width: 100%;
-}
-h1 {
-  margin: 0;
 }
 </style>
