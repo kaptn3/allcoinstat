3586e9b2d5486201bafdde9326fea8acd3336987
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-23 18:07:51 +0300

    Tags

---
+++ src/components/AppTags.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="AppTags">
+    <a v-for="(value, key) in tags" :key="key" class="tag" href="value.link"># {{ key }}</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppTags',
+    data () {
+      return {
+        tags: {
+          tag: {
+            link: '/'
+          },
+          longtag: {
+            link: '/'
+          },
+          longtags: {
+            link: '/'
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .AppTags {
+    display: flex;
+  }
+  .tag,
+  .tag:visited {
+    font-weight: bold;
+    transition: .3s all ease;
+    padding: 6px 10px;
+    border-radius: 100px;
+    color: var(--blue-color);
+    border: 1px solid var(--blue-color);
+  }
+  .tag:hover,
+  .tag:focus {
+    color: #fff;
+    background-color: var(--blue-color);
+  }
+</style>
