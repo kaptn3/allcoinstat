9cf547b64af576b8895ef2be2b859246f5bd7b0e
vikapitoshka <kapitonenko.vick@ya.ru>
2018-04-08 12:43:24 +0300

    Fix hover few table

--- src/assets/js/table.js
+++ src/assets/js/table.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event){
   document.onmouseover = function (e) {
     if (e.target.nodeName === 'TH') {
-      let column = document.querySelectorAll('td.' + e.target.className);
+      let data = e.target.parentElement.parentElement.parentElement.dataset.table;
+      let table = document.querySelector('[data-table=' + data + ']');
+      let column = table.querySelectorAll('td.' + e.target.className);
       for (let i = 0; i < column.length; i++) {
         column[i].classList.add('td-hover');
       }
@@ -9,7 +11,9 @@ document.addEventListener("DOMContentLoaded", function(event){
   }
   document.onmouseout = function (e) { 
     if (e.target.nodeName === 'TH') {
-      let column = document.querySelectorAll('td.' + e.target.className);
+      let data = e.target.parentElement.parentElement.parentElement.dataset.table;
+      let table = document.querySelector('[data-table=' + data + ']');
+      let column = table.querySelectorAll('td.' + e.target.className);
       for (let i = 0; i < column.length; i++) {
         column[i].classList.remove('td-hover');
       }
