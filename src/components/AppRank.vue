cf398fe66d224194ee952d609188927faedae026
vikapitoshka <kapitonenko.vick@ya.ru>
2018-04-08 16:06:59 +0300

    Add rank of currency

---
+++ src/components/AppRank.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="AppRank">
+    <slot>-</slot> Rank
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'AppRank'
+  }
+</script>
+
+<style scoped>
+  .AppRank {
+    margin-left: 10px;
+    font-weight: 500;
+    text-align: center;
+    padding: 6px 15px;
+    border-radius: 100px;
+    border: solid 2px var(--light-color);
+  }
+</style>
